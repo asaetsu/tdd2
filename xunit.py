60ac77ba75da5a1e6de18cc0c9059b4af00545ac
vagrant <asaetsu0130@gmail.com>
2018-01-28 05:09:21 +0000

    Chapter22

--- xunit.py
+++ xunit.py
@@ -12,8 +12,11 @@ class TestCase:
         result = TestResult()
         result.testStarted()
         self.setUp()
-        method = getattr(self, self.name)
-        method()
+        try:
+            method = getattr(self, self.name)
+            method()
+        except:
+            result.testFailed
         self.tearDown()
         return result
 
@@ -45,17 +48,28 @@ class TestCaseTest(TestCase):
         test = WasRun("testBrokenMethod")
         result = test.run()
         assert("1 run, 1 failed" == result.summary()) 
+    
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert("1 run, 1 failed" == result.summary())
 
 class TestResult:
     def __init__(self):
         self.runCount = 0
+        self.errorCount = 0
 
     def testStarted(self):
         self.runCount = self.runCount + 1
 
-    def summary(self):
-        return "%d run, 0 failed" % self.runCount
+    def testFailed(self):
+        self.errorCount = self.errorCount + 1
 
-TestCaseTest("testTemplateMethod").run()
-TestCaseTest("testResult").run()
-#TestCaseTest("testFailedResult").run()
+    def summary(self):
+        return "%d run, %d failed" % (self.runCount, self.errorCount)
+
+print(TestCaseTest("testTemplateMethod").run().summary())
+print(TestCaseTest("testResult").run().summary())
+print(TestCaseTest("testFailedResult").run().summary())
+print(TestCaseTest("testFailedResultFormatting").run().summary())
